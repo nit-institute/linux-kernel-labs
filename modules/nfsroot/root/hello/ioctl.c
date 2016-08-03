d3620496486b66eefe373d95c13c6de688ed60e1
Nemanja Fimic <nemanja.fimic@rt-rk.com>
2016-08-03 17:38:09 +0200

    Add ioctl.c file to hello module. this file is needed for testing ioctl functionality in hello_driver.

---
+++ modules/nfsroot/root/hello/ioctl.c
@@ -0,0 +1,38 @@
+/* Simple program to send ioctl commands to a device, from the command line
+ * Usage: ioctl <device> <cmd>
+ * Example: ioctl /dev/foo 3
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+int main (int argc, char *argv[])
+{
+
+	int file;
+	int cmd; 
+
+	if (argc != 3) {
+		fprintf(stderr, "ioctl: wrong number of arguments\n");
+		exit(1);
+	}
+
+	sscanf (argv[2], "%i", &cmd);
+
+  	if ((file = open(argv[1], O_RDONLY)) < 0) {
+		fprintf(stderr, "Error opening file %s\n", argv[1]);
+		exit(1);
+  	}
+
+	if (ioctl(file, cmd)) {
+		fprintf(stderr, "Error sending the ioctl command %d to file %s\n", cmd, argv[1]);
+		exit(1);
+  	}
+		
+	close(file);
+	return 0;
+}
+	

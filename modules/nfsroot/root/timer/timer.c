4003d0b31a77a676f5bc21b2dd46bcfe0b3b49ee
Vladimir Marinkovic <Vladimir.Marinkovic@rt-rk.com>
2016-12-14 13:34:45 +0100

    dodata postavka za timer vezbu
    Signed-off-by: Vladimir Marinkovic <Vladimir.Marinkovic@rt-rk.com>

---
+++ modules/nfsroot/root/timer/timer.c
@@ -0,0 +1,130 @@
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_irq.h>
+#include <linux/platform_device.h>
+#include <linux/io.h>
+#include <linux/pm_runtime.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/interrupt.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+
+#define REG_CONTROL	0x00
+#define REG_COUNTER_LO	0x04
+#define REG_COUNTER_HI	0x08
+#define REG_COMPARE(n)	(0x0c + (n) * 4)
+#define MAX_TIMER	3
+#define DEFAULT_TIMER	1
+
+#define TIMER_SEC 1
+
+struct timer_dev {
+	void __iomem *regs;
+};
+
+static unsigned int reg_read(struct timer_dev *dev, int offset)
+{
+	return readl(dev->regs + offset);
+}
+
+static void reg_write(struct timer_dev *dev, int value, int offset)
+{
+	writel(value, dev->regs + offset);
+}
+
+static irqreturn_t timer_irq(int irq, void *dev_id)
+{
+	struct timer_dev *dev = dev_id;
+	unsigned int old_value;
+
+	/* clear timer match detect status bit */
+
+
+	/* set timer compare value to (old_value + timer_ticks) */
+
+
+
+	return IRQ_HANDLED;
+}
+
+static ssize_t timer_read(struct file *file, char __user *buf,
+			     size_t sz, loff_t *ppos)
+{
+	int ret;
+	unsigned int cs, lo, hi, c0, c1, c2, c3;
+	unsigned int len, to_copy;
+	
+	return to_copy;
+}
+
+static ssize_t timer_write(struct file *file, const char __user *buf,
+			      size_t sz, loff_t *ppos)
+{
+
+	return 0;
+}
+
+static const struct file_operations timer_fops = {
+	.owner = THIS_MODULE,
+	.write = timer_write,
+	.read = timer_read,
+};
+
+static int timer_probe(struct platform_device *pdev)
+{
+
+	/* Enable power management */
+
+
+	/* read timer freq */
+
+
+	/* Initialize wait queue */
+
+
+	/* Get IRQ number from the Device Tree */
+
+
+	/* clear timer match detect status bit */
+
+
+	/* Register interrupt handler */
+
+
+	/* set timer compare value to (Timer Counter Lower + timer_ticks) */
+
+
+	/* Declare misc device */
+
+
+	return 0;
+}
+
+static int timer_remove(struct platform_device *pdev)
+{
+
+	return 0;
+}
+
+static struct of_device_id timer_dt_match[] = {
+	{ .compatible = "rtrk,timer" },
+	{ },
+};
+
+static struct platform_driver timer_driver = {
+	.driver = {
+		.name = "timer",
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(timer_dt_match),
+	},
+	.probe = timer_probe,
+	.remove = timer_remove,
+};
+
+module_platform_driver(timer_driver);
+MODULE_LICENSE("GPL");
